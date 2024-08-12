61b1dc5a5e0df3aadb72a923e84def1a4decfee9
Usagi no Niku <chenbohan911@163.com>
2024-08-12 09:30:03 +0800

    project: re-track files

--- backend/src/pyfuta/app/defs/pages.py
+++
@@ -1,5 +0,0 @@
-from pyfuta.app.pages.builder import PageBuilder
-from pyfuta.app.defs.reports import *
-
-PageBuilder(path="products", title="商品销售", description="商品销售情况汇总信息").reports(*products)
-PageBuilder(path="consumers", title="商品客户", description="商品客户情况汇总信息").reports(*customers)
